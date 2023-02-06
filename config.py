7889a91ddfb4219d1d338f4649abaf69dd252f39
BaldanovErdem <124584360+BaldanovErdem@users.noreply.github.com>
2023-02-07 00:36:17 +1100

    Публикация для защиты

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a.a_4z8cTot1d0W_WC7k-GK2dYwfVt2ZGwA8NiV7W8Xuq338TuBNeQc21FhZsD_n5frWxjigvS6SmWTxIGF-XZNUZXz_I1LU2PVFN1ypi-fi_OeyGKVSPTqZd3pmBm5sjLQ-T9tagYv-Ry37VWcIch5YaQLy7fmDWhxm-56AFu9nA2XhHqz1wdmLsSu5ELdgSelbSjjD6xxfnkCFAHdIi8TA' # токен пользователя
+comm_token = 'vk1.a.PPgleZKqltg-lXTeZqGwOBGxFHlWtqvSN8GoVSPhcBTUYe3gzcCa9K0SjlIf99e0Um27DClB4Z7HZ4Y11xFQYTsYdJ4q0uc_SmH3c68DHFGUwe_j9kgW4B5tX5LH9lsKsL2P28dlaNqfG7dbKbHG-vbXGIj_o9CuI-ejmXqNJvWoWis55PPA0lmWMOWqRJtGgv1gNe4IM1cwHZN1R3I2rA' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '1ege_80RUS'
+db_name = 'bot_users'
