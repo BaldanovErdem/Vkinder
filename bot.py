bdd9c797401394ed883d56b29818ff23df45d892
BaldanovErdem <124584360+BaldanovErdem@users.noreply.github.com>
2023-02-16 01:36:34 +1100

    Публикация для защиты V2

--- bot.py
+++ bot.py
@@ -8,18 +8,19 @@ for event in bot.longpoll.listen():
         user_id = str(event.user_id)
         msg = event.text.lower()
         sender(user_id, msg.lower())
+        first_name, sex, bdate, city = bot.get_user_info(user_id)
         if request == 'поиск':
             creating_database()
-            bot.write_msg(user_id, f'Привет, {bot.name(user_id)}')
-            bot.find_user(user_id)
-            bot.write_msg(event.user_id, f'Нашёл для тебя пару, жми на кнопку "Вперёд"')
+            bot.write_message(user_id, f'Привет, {first_name}')
+            bot.find_insert_users(user_id)
+            bot.write_message(event.user_id, f'Нашёл для тебя пару, жми на кнопку "Далее"')
             bot.find_persons(user_id, offset)
 
         elif request == 'далее':
-            for i in line:
+            for i in range(0, 1000):
                 offset += 1
                 bot.find_persons(user_id, offset)
                 break
 
         else:
-            bot.write_msg(event.user_id, 'Твоё сообщение непонятно')
+            bot.write_message(event.user_id, 'Твоё сообщение непонятно')
